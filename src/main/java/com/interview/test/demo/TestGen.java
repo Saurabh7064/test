f2e702e0146706a26961f29ae65813595ce3c8f1
emosrb <emosrb@gmail.com>
2023-06-19 13:42:13 -0500

    Few more generics example

--- src/main/java/com/interview/test/demo/TestGen.java
+++ src/main/java/com/interview/test/demo/TestGen.java
@@ -5,6 +5,8 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 //Learning Generics
 @Data
 @AllArgsConstructor
@@ -58,6 +60,80 @@ class MyGenList<T>  {
     //Here String a just a keyword like foo bar not the actual String class
 }
 
+class Box<T> {
+    private T contents;
+
+    public Box(T contents) {
+        this.contents = contents;
+    }
+
+    public T getContents() {
+        return contents;
+    }
+
+    public void setContents(T contents) {
+        this.contents = contents;
+    }
+}
+class Pair<T, U> {
+    private T first;
+    private U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair [first="+first + ", second="+second + "]";
+    }
+}
+
+class Stack<T> {
+    private ArrayList<T> elements;
+
+    public Stack() {
+        elements = new ArrayList<>();
+    }
+
+    public void push(T item) {
+        elements.add(item);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack is empty.");
+        }
+        return elements.remove(elements.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    public int size() {
+        return elements.size();
+    }
+}
+
+
 public class TestGen {
 
     public static void main(String[] args) {
@@ -83,6 +159,34 @@ public class TestGen {
         myGenList.addToList(student5);
 
         myGenList.getList().stream().forEach(System.out::println);
+
+        //BOX class
+        Box<String> box = new Box<>("Hello, World");
+        Box<Integer> box2 = new Box<>(10);
+        System.out.println(box.getContents());
+
+        //Pair Class
+        Pair pair = new Pair("John Doe", 25);
+        Pair pair2 = new Pair(10,20);
+        Pair pair3 = new Pair("Saurabh","Kumar");
+
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+        System.out.println(pair2.getFirst());
+        System.out.println(pair2.getSecond());
+        System.out.println(pair3.getFirst());
+        System.out.println(pair3.getSecond());
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(30);
+        stack.pop();
+
+        Stack<String> stack2 = new Stack<>();
+        stack2.push("using");
+        stack2.push("C");
+        System.out.println(stack);
+        System.out.println(stack2);
 
      }
   }
